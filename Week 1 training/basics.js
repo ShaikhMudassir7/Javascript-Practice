f1efdf074a5a0230334c289c86a44f5a36ae3fdd
shaikhmudassir7 <send2mudassir@gmail.com>
2023-06-09 11:54:31 +0100

    basic

---
+++ Week 1 training/basics.js
@@ -0,0 +1,22 @@
+// Basics
+// JavaScript borrows most of its syntax from Java, C, and C++, but it has also been influenced by Awk, Perl, and Python.
+
+// JavaScript is case-sensitive and uses the Unicode character set. For example, the word वस्तु (which means "thing" in Hindi) could be used as a variable name.
+const वस्तु= "Thing";
+
+// the variable früh is not the same as Früh because JavaScript is case sensitive.
+
+// In JavaScript, instructions are called statements and are separated by semicolons (;).
+
+// A semicolon is not necessary after a statement if it is written on its own line. But if more than one statement on a line is desired, then they must be separated by semicolons.
+
+// It is considered best practice, however, to always write a semicolon after a statement, even when it is not strictly needed. This practice reduces the chances of bugs getting into the code.
+
+// The source text of JavaScript script gets scanned from left to right, and is converted into a sequence of input elements which are tokens, control characters, line terminators, comments, or whitespace. (Spaces, tabs, and newline characters are considered whitespace.)
+
+let int = 1;
+let string= "Hello, world!";
+let float = true;
+var str= "Hello, world!";
+
+console.log(int +" " + string+" " + float+" " + str+ " "+वस्तु);
